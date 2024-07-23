b48b6c60b1530557c97f628bf369633019938908
Driijko <driijko@gmail.com>
2024-07-23 10:52:09 -0400

    static: playlist.

---
+++ src/static/playlist.js
@@ -0,0 +1,22 @@
+const playlist = [
+  { 
+    name: "Buttercup", 
+    path: "./audio/buttercup.mp3",
+  },
+  {
+    name: "Chirpy",
+    path: "./audio/chirpy.mp3",
+  },
+  {
+    name: "Corridors",
+    path: "./audio/corridors.mp3",
+  },
+  {
+    name: "Fade",
+    path: "./audio/fade.mp3",
+  },
+  {
+    name: "My Bloody Disco",
+    path: "./audio/mybloodydisco.mp3"
+  },
+];

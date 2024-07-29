8d44b79e4db15bb343747b5a68ff0c88b5425ebc
Driijko <driijko@gmail.com>
2024-07-29 18:39:28 -0400

    Timer.

---
+++ src/components/5-elements/Timer.svelte
@@ -0,0 +1,26 @@
+<!-- SCRIPTS ////////////////////////////////////// -->
+<script>
+  // IMPORTS -----------------------------------
+  import { currentTime, totalTime} 
+  from "../../dynamic/audio"
+  
+</script>
+
+<!-- MARKUP //////////////////////////////////// -->
+{#if $totalTime > 0}
+<span class="audio-bkg-timer">
+  <span>
+    {Math.floor($currentTime / 60)}:{
+    Math.floor($currentTime % 60) < 10 ? 
+    `0${Math.floor($currentTime % 60)}`
+    : Math.floor($currentTime % 60)}
+  </span>
+  <span> / </span>
+  <span>
+    {Math.floor($totalTime / 60)}:{
+      Math.floor($totalTime % 60) < 10 ? 
+      `0${Math.floor($totalTime % 60)}`
+      : Math.floor($totalTime % 60)}
+  </span>
+</span>
+{/if}
--- src/dynamic/audio.js
+++ src/dynamic/audio.js
@@ -4,6 +4,8 @@ import { writable, get } from "svelte/store";
 export const ctpi = writable(0); // current track playlist index
 export const audioPaused = writable(true);
 export const volume = writable(0);
+export const currentTime = writable(0);
+export const totalTime = writable(0);
 export const currentTrack = writable({
   name: "Buttercup",
   path: "./audio/buttercup.mp3",

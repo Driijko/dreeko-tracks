15498bd14eaf59b2a05bac31fc317fd63ce59904
Driijko <driijko@gmail.com>
2024-08-02 13:10:33 -0400

    backgrounds: optimize buttercup background image.

--- src/static/playlist.js
+++ src/static/playlist.js
@@ -18,10 +18,10 @@ const playlist = [
     path: "./audio/chirpy.mp3",
     colors: {
       color1: "hsl(300, 100%, 50%)",
-      color1T: "hsla(0, 0%, 100%, 0.5)",
-      color1D: "hsl(0, 0%, 70%)",
+      color1T: "hsla(300, 100%, 50%, 0.5)",
+      color1D: "hsl(300, 40%, 50%)",
       color2: "hsl(150, 100%, 50%)",
-      color2T: "hsla(0, 0%, 0%, 0.8)",
+      color2T: "hsla(150, 100%, 70%, 0.8)",
       color2T2: "hsla(0, 0%, 0%, 0.5)",
       color2T3: "hsla(0, 0%, 0%, 0.9)",
       color2D: "hsl(0, 0%, 20%)",
